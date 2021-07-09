91314f23c709f61cf31d0081bd73a64e2af91572
Matt Hill <matthewonthemoon@gmail.com>
2021-07-09 14:37:16 -0600

    our own json patch lib and everything working

--- client/index.ts
+++ client/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/source/poll-source'
 export * from './lib/source/ws-source'
 export * from './lib/source/source'
+export * from './lib/json-patch-lib'
 export * from './lib/patch-db'
 export * from './lib/store'
 export * from './lib/types'
